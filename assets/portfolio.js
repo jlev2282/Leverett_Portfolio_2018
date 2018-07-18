0d2793d3032e6fc9063fe001ed9bde2fb8b4dcd7
John Leverett <jleverett@gls.com>
2018-07-18 16:56:22 -0400

    base function to add data to menu

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -5,25 +5,37 @@ $(document).ready(function(){
     //define the functions to use populatePage, populateMenu, populateContent
     //
 
-    var menuArray = [
+
+    var mainOptions = [
         {
-            level: 1,
+            level: "main-options",
             label: "About me",
             glyph: "fas fa-user-astronaut",
+            href: "About"
         },
         {
-            level: 1,
+            level: "main-options",
             label: "Portfolio",
             glyph: "fas fa-folder",
+            href: "Portfolio"
+
         },
         {
-            level: 1,
+            level: "main-options",
             label: "Contact Me",
             glyph: "fas fa-envelope",
+            href: "Contact"
+
         }
-
-
     ];
+    var portfolioOptions = [
+
+        ];
+
+    var menuArrays = [ mainOptions, portfolioOptions];
+
+
+    var pageContent = [];
 
     var page = ["About", "Portfolio", "Contact"];
 
@@ -66,22 +78,48 @@ $(document).ready(function(){
 
     //a for loop that grabs each index from menuArray and creates a div with class "row"
     //creates
-    function populateMenu(page){
-        for (i=0; i< menuArray.length; i++) {
-            var newRow = $("<div>");
+    function populateMenu(array){
+        console.log("I'm here");
+
+        for (i=0; i< array.length; i++) {
+            //creates menu options
+            if (array[i] == mainOptions) {
+                var $newlist = $("<ul>");
+                menuOptions = array[i];
+                for (j =0; j < menuOptions.length; j++) {
+                    var listItem = $("<li>");
+                    var listItemP = $("<p>");
+                    var listItemA = $("<a>");
+                    var listItemI = $("<i>");
+
+                    listItemI.attr("class", menuOptions[j]['glyph']);
+                    listItemA.html(listItemI);
+                    listItemA.attr("href", menuOptions[j]['href']);
+                    listItemP.html(listItemA+menuOptions[j]['label']);
+                    listItem.html(listItemP);
+                    $newlist.html(listItem);
+                    $("#main-options").append($newlist);
+
+                    console.log(listItem);
+
+                }
+
+
+            }
         }
 
     }
 
-    function populateContent(page){
-
+    function populateContent(content){
+        return true;
     }
 
-    function populatePage(page){
-        populateMenu(page);
-        populateContent(page);
+    function populatePage(array, content){
+        populateMenu(array);
+        populateContent(content);
     }
 
-    populatePage();
+    // populatePage(menuArrays, pageContent);
+
 
 });
