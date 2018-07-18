6614439968194de916eac9ff7d71989dc8ffbea9
John <jlev2282@gmail.com>
2018-07-17 22:32:48 -0400

    outline of functions that populate page

---
+++ assets/portfolio.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+//define the things to populate
+var menuArray = [
+    {
+        level: 1,
+        label: "About me",
+        glyph: "",
+    },
+    {
+        level: 1,
+        label: "Portfolio",
+        glyph: "",
+    },
+    {
+        level: 1,
+        label: "Contact Me",
+        glyph: "", 
+    }
+
+
+];
+
+function populateMenu(){
+
+};
+
+function populateContent(){
+
+};
+
+function populatePage(){
+    populateMenu();
+    populateContent();
+};
+
+populatePage();
+
+});
